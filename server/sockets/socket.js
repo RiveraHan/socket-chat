c5d8eecf566075c35db8daf36a6592bc0e9f5818
Hanzell Rivera <hanzellrivera95@gmail.com>
2020-10-05 21:14:40 -0600

    Chat version Beta

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -21,23 +21,26 @@ io.on('connection', (client) => {
         const personas = usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
         client.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${ data.nombre } se unió`));
 
         return callback(usuarios.getPersonasPorSala(data.sala));
     });
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
 
         const persona = usuarios.getPersona(client.id);
 
         const mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
     });
 
     client.on('disconnect', () => {
 
         const personaBorrada = usuarios.borrarPersona(client.id);
 
-        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Adminitrador', `${ personaBorrada.nombre } salió`));
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salió`));
         client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
     });
 
